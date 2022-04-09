cb60ea8f2c9ee8c8a099dcb017821fbc13a38626
tzelginianc <tzelginia78@gmail.com>
2022-04-09 20:37:00 +0700

    menambahkan fungsi untuk relasi kelas

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model; //Model Eloquen
-use App\Models\Mahasiswa;
+// use App\Models\Mahasiswa;
 class Mahasiswa extends Model
 {
     protected $table='mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
@@ -21,11 +21,11 @@ class Mahasiswa extends Model
  'kelas_id',
  'jurusan',
  ];
-  //menambah 3 kolom pada model
-//   'Email' ,
-//   'Alamat' ,
-//   'tanggal_lahir' ,
 public function kelas(){
     return $this->belongsTo(Kelas::class);
 }
+public function mahasiswa_matakuliah()
+    {
+        return $this->hasMany(Mahasiswa_MataKuliah::class, 'mahasiswa_id', 'nim');
+    }
 }
