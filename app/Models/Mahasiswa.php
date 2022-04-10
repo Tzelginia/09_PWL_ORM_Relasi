e6a7d4bada338c9772e170007be970e75b4b6faf
tzelginianc <tzelginia78@gmail.com>
2022-04-10 23:04:18 +0700

    model mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Contracts\Auth\MustVerifyemail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
@@ -8,23 +10,27 @@ use Illuminate\Database\Eloquent\Model; //Model Eloquen
 // use App\Models\Mahasiswa;
 class Mahasiswa extends Model
 {
-    protected $table='mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
- protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
- /**
- * The attributes that are mass assignable.
- *
- * @var array
- */
- protected $fillable = [
- 'nim',
- 'nama',
- 'kelas_id',
- 'jurusan',
- ];
-public function kelas(){
-    return $this->belongsTo(Kelas::class);
-}
-public function mahasiswa_matakuliah()
+    protected $table = 'mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+    protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas_id',
+        'jurusan',
+        'Email',
+        'Alamat',
+        'tanggal_lahir',
+    ];
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class);
+    }
+    public function mahasiswa_matakuliah()
     {
         return $this->hasMany(Mahasiswa_MataKuliah::class, 'mahasiswa_id', 'nim');
     }
